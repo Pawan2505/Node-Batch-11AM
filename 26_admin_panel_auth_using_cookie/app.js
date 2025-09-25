1d7196b51dd6de48ff0c0addd4551d6f85d22d95
pawan2505 <pawanaktu2gmail.com>
2025-09-25 19:36:01 +0530

    auth using cookie

---
+++ 26_admin_panel_auth_using_cookie/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('./config/db');
+const port = 8000;
+const path = require('path');
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'assets')));
+app.use('/uploads',express.static(path.join(__dirname,'uploads/')));
+
+app.use(express.urlencoded({extended:true}));
+
+app.use('/',require('./routes/index'));
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }else{
+        console.log("server start on port :",port);
+    }
+})
